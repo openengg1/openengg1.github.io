85105133fb75b8488df0535ce75daa7098e975b0
Rohit Mishra <rohitmishranitrr@gmail.com>
2025-09-18 11:13:23 -0500

    Update main.go

--- resume/main.go
+++ resume/main.go
@@ -10,28 +10,38 @@ import (
 )
 
 func main() {
-	jsonData, _ := ioutil.ReadFile("./profile_resume.json")
+	jsonData, err := ioutil.ReadFile("./profile_resume.json")
+	if err != nil {
+		log.Fatalf("Error reading profile_resume.json: %v", err)
+	}
 	profileio.ProfileIO(jsonData)
 
-	app := "xelatex"
-	if !commandExists(app) {
-		app = "pdflatex"
-		if !commandExists(app) {
-			log.Print("Cannot compile resume.tex")
-			os.Exit(0) // Exit with this code to avoid github actions failure.
-		}
-	}
+	// The following code attempts to compile the resume using pdflatex/xelatex.
+	// This is already handled by the GitHub Actions workflow in later steps.
+	// We are commenting it out to prevent the "basic.tmpl" file not found error,
+	// as the core function of this Go program (creating the .tex file from json)
+	// is already complete at this point.
 
-	os.Chdir("./resume")
-	cmd := exec.Command(app, "resume.tex")
-	_, err := cmd.Output()
-	if err != nil {
-		log.Printf("Error: could not process TeX file, %v", err)
-	}
+	/*
+		app := "xelatex"
+		if !commandExists(app) {
+			app = "pdflatex"
+			if !commandExists(app) {
+				log.Print("Cannot compile resume.tex")
+				os.Exit(0) // Exit with this code to avoid github actions failure.
+			}
+		}
+
+		os.Chdir("./resume")
+		cmd := exec.Command(app, "resume.tex")
+		_, err = cmd.Output()
+		if err != nil {
+			log.Printf("Error: could not process TeX file, %v", err)
+		}
+	*/
 }
 
 func commandExists(cmd string) bool {
 	_, err := exec.LookPath(cmd)
-
 	return err == nil
 }
